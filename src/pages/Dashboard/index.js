114c538c8af8b1492cef5a8f67593f3fb0106db1
jul1ana <julianaadt97@gmail.com>
2022-12-04 16:13:58 -0300

    Implementing the CRUD List in the system

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,18 @@
 import React, { useContext } from "react";
 import { Context } from "../../Context/AuthContext";
+import { Link } from "react-router-dom";
 
 export const Dashboard = () => {
-
-  const token = localStorage.getItem("token");
 
   const { authenticated, handleLogout } = useContext(Context);
   console.log("User status on dashboard page: " + authenticated);
 
   return (
     <div>
+      <Link to="/dashboard">Dashboard</Link> <br />
+      <Link to="/users">Users</Link> <br />
+
       <h1>Dashboard</h1>
-      <p>Token: {token}</p>
       <button type="button" onClick={handleLogout} >Exit</button>
     </div>
   );
--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../../config/configApi";
 
 export const Users = () => {
+
+  const [data, setData] = useState([]);
+
+  const [status, setStatus] = useState({
+    type: "",
+    message: ""
+  });
+
+  const getUsers = async () => {
+    const headers = {
+      "headers": {
+        "Authorization": "Bearer " + localStorage.getItem("token")
+      }
+    }
+
+    await api.get("/users", headers)
+      .then((response) => {
+        //recebendo os dados da API
+        setData(response.data.users);
+      }).catch((err) => {
+        if (err.response) {
+          setStatus({
+            type: "error",
+            message: err.response.data.message
+          });
+        } else {
+          setStatus({
+            type: "error",
+            message: "ERROR: Try later!"
+          });
+        }
+      });
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <>
+      <Link to="/dashboard">Dashboard</Link> <br />
+      <Link to="/users">Users</Link> <br />
+
       <h1>List Users</h1>
+
+      {status.type === "error" ? <p>{status.message}</p> : ""}
+
+      {data.map(user => (
+        <div key={user.id}>
+          <span>{user.id}</span> <br />
+          <span>{user.name}</span> <br />
+          <span>{user.email}</span> <br /> <hr />
+        </div>
+      ))}
     </>
   );
 }
